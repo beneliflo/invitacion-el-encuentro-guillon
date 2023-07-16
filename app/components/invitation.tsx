84ac8da377a98fc79c9a89433b8d1e085d6fcb73
Benjamin Flores <beneliflo@gmail.com>
2023-07-16 05:32:17 -0300

    update

--- app/components/invitation.tsx
+++ app/components/invitation.tsx
@@ -5,7 +5,7 @@ import cn from 'classnames'
 // import Image from 'next/image'
 // import Link from 'next/link'
 // import { Fade } from "react-awesome-reveal";
-// import localFont from 'next/font/local'
+import localFont from 'next/font/local'
 // import { Titillium_Web } from 'next/font/google'
 
 import styles from '../styles.module.css'
@@ -17,20 +17,35 @@ import styles from '../styles.module.css'
 // })
  
 
-// const snellRoundhand = localFont({
-//   src: [
-//     {
-//       path: '../fonts/snell-roundhand/SnellBT-Regular.otf',
-//       weight: '400',
-//       style: 'normal',
-//     },
-//     {
-//       path: '../fonts/snell-roundhand/SnellRoundhand-BoldScript.otf',
-//       weight: '700',
-//       style: 'normal',
-//     },
-//   ],
-// })
+const corporateACondPro = localFont({
+  src: [
+    {
+      path: '../fonts/corporate-a-pro/CorporateACondPro-Light.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/corporate-a-pro/CorporateACondPro-Bold.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+})
+
+const sherika = localFont({
+  src: [
+    {
+      path: '../fonts/sherika/Sherika-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/sherika/Sherika-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+})
 
 export default function Invitation() {
   const [open, setOpen] = useState(false);
@@ -57,7 +72,7 @@ export default function Invitation() {
   return (
     <div className={cn(`bg-[url('/bg.png')] bg-cover bg-no-repeat h-screen bg-top`, letter ? 'overflow-auto' : 'overflow-hidden')}>
       {!letter &&
-        <div className="flex items-center justify-center w-full h-screen overflow-hidden animate-zoomFadeIn">
+        <div className="flex items-center justify-center w-full h-screen overflow-hidden animate-zoomFadeIn font">
           <div className={cn(styles.envelopeWrapper, open ? styles.open : styles.close)}>
             <div className={cn('sm:scale-150', styles.envelope, open ? styles.open : styles.close)}>
               <div className={cn(styles.front, styles.flap)}></div>
@@ -73,19 +88,23 @@ export default function Invitation() {
         </div>
       }
       {letter &&
-        <div className="max-w-[800px] mx-auto animate-slideFadeUp p-4 md:pt-72">
+        <div className="max-w-[832px] mx-auto animate-slideFadeUp p-4 md:pt-[227px]">
           <div className="overflow-hidden bg-white rounded-lg">
-            <div className="relative flex flex-col p-6 sm:p-10">
-                32 AÑOS
-                Celebraremos juntos
-                de la fidelidad de dios
-                pacto•Unidad•Frutos
-                reservá las fechas
-                Sábado
-                19hs.
-                Domingo
-                18hs.
-                ¡Agendá estos días de fiesta y disfrutemos el regalo de ser iglesia!
+            <div className="relative flex flex-col">
+              <div className="bg-[url('/bg-top.png')] bg-contain bg-no-repeat bg-top text-center pt-[150px] pb-20">
+                <p className={cn(sherika.className, 'uppercase font-medium text-2xl bg-[#C7FD01] p-[10px] w-max mx-auto tracking-wider -mb-2')}>Celebraremos juntos</p>
+                <h2 className={cn(corporateACondPro.className, 'uppercase font-medium text-[180px] leading-none')}>32 AÑOS</h2>
+                <p className={cn(sherika.className, 'uppercase font-normal text-[40px] tracking-wider -mt-4')}>de la fidelidad de dios</p>
+              </div>
+              <div className="bg-[url('/bg-sky.png')] bg-cover bg-no-repeat text-center font-">
+                <p className={cn(corporateACondPro.className, 'font-extralight text-[105px] leading-snug tracking-tight')}>Pacto•Unidad•Frutos</p>
+              </div>
+              <div className="sm:px-20 bg-[url('/bg-bottom.png')] bg-contain bg-no-repeat bg-bottom pb-[280px] pt-[100px]">
+                <p className={cn(sherika.className, 'uppercase font-medium text-2xl bg-[#C7FD01] p-[10px] w-max tracking-wider')}>reservá las fechas</p>
+                <h2 className="flex justify-between items-end border-b-2 border-black mt-10"><span className={cn(corporateACondPro.className, 'font-medium text-8xl')}>Sábado</span><span className={cn(sherika.className, 'uppercase font-normal text-[52px] leading-[58px] tracking-wider')}>19hs.</span></h2>
+                <h2 className="flex justify-between items-end border-b-2 border-black mt-10"><span className={cn(corporateACondPro.className, 'font-medium text-8xl')}>Domingo</span><span className={cn(sherika.className, 'uppercase font-normal text-[52px] leading-[58px] tracking-wider')}>18hs.</span></h2>
+                <p className={cn(sherika.className, 'text-center font-normal text-[40px] mt-20')}>¡Agendá estos días de fiesta y disfrutemos el regalo de ser iglesia!</p>
+              </div>
             </div>
           </div>
         </div>
